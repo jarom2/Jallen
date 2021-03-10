014a8072a1baf732f31c5fc8feddb31b9d7fcd5d
Jarom Allen <36427376+jarom2@users.noreply.github.com>
2021-03-10 10:28:31 -0700

    Update and rename src/main2.cpp to src/PEX8/main2.cpp

--- src/PEX8/main2.cpp
+++ src/PEX8/main2.cpp
@@ -3,25 +3,25 @@
 #include <iostream>
 int main()
 {
-ListKeyVal<std::string, ListKeyVal<std::string, int> > outer;
-ListKeyVal<std::string, int> lkv;
-//populate an LKV
-lkv.insert("one", 1);
-lkv.insert("three", 3);
-lkv.insert("four", 4);
-lkv.insert("two", 2);
-//put two copies map into outer
-outer.insert("outer1", lkv);
-outer.insert("outer2", lkv);
-//iterate over outer then inner
-std::cout << std::endl << std::endl << "Full iteration:" << std::endl;
-outer.forEach([](const std::string &key, ListKeyVal<std::string, int> 
-&val){
-std::cout << key << std::endl;
-val.forEach([](const std::string &key, int &val){
-std::cout << "  Key: " << key << std::endl;
-std::cout << "  Val: " << val << std::endl;
-});
+  ListKeyVal<std::string, ListKeyVal<std::string, int> > outer;
+  ListKeyVal<std::string, int> lkv;
+  //populate an LKV
+  lkv.insert("one", 1);
+  lkv.insert("three", 3);
+  lkv.insert("four", 4);
+  lkv.insert("two", 2);
+  //put two copies map into outer
+  outer.insert("outer1", lkv);
+  outer.insert("outer2", lkv);
+  //iterate over outer then inner
+  std::cout << std::endl << std::endl << "Full iteration:" << std::endl;
+  outer.forEach([](const std::string &key, ListKeyVal<std::string, int> 
+  &val){
+  std::cout << key << std::endl;
+  val.forEach([](const std::string &key, int &val){
+  std::cout << "  Key: " << key << std::endl;
+  std::cout << "  Val: " << val << std::endl;
+  });
 });
 //make a copy of one LKV, delete an item, then iterate
 std::cout << std::endl << std::endl << "Deleted item \"three\":" << 
